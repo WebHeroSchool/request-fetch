5dc72a80b3d19acea724b2b154e2591a77950278
Regina <saifieva98@gmail.com>
2020-08-23 11:05:59 +0300

    get img name bio

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var newDiv = document.createElement('div');
+var body = document.body;
 fetch('https://api.github.com/users/saifieva98')
-	.then(res => res.json)
-	.then(json => newDiv.innerHTML = json.name)
-	.then(json => newDiv.innerHTML = json.bio)
-	.then(json => newDiv.innerHTML = json.avatar_url)
+	.then(res => res.json())
+	.then(json => {
+   	let name = document.createElement('h1');
+   	name.innerHTML = json.name;
+   	body.append(name);
+   	name.addEventListener("click", () => window.location = json.html_url);
+   	let description = document.createElement('p');
+   	description.innerHTML = json.bio;
+   	body.append(description);
+   	let img = new Image();
+   	img.src = json.avatar_url;
+   	body.append(img);
+	})
 	.catch(err => console.log(err));
