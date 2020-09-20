8c759c76f837d17d7fa9ad164fdbea0007ed9efd
Regina <saifieva98@gmail.com>
2020-09-20 22:18:41 +0300

    modificate univarsal script

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ fetch('https://api.github.com/users/saifieva98')
 	.then(res => res.json())
 	.then(json => {
    	let name = document.createElement('h1');
-   	name.innerHTML = json.name;
+   	if (json.name != null) {
+         name.innerHTML = json.name;
+      } else {
+         name.innerHTML = 'Данные отсутствуют';
+      }
    	body.append(name);
    	name.addEventListener("click", () => window.location = json.html_url);
    	let description = document.createElement('p');
-   	description.innerHTML = json.bio;
+   	if (json.bio != null) {
+         description.innerHTML = json.bio;
+      } else {
+         description.innerHTML = 'Данные отсутствуют';
+      }
    	body.append(description);
    	let img = new Image();
    	img.src = json.avatar_url;
