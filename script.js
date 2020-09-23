c77d3b8dc3ff58f0f9082b6452484933789ab17e
Regina <saifieva98@gmail.com>
2020-09-23 06:58:41 +0300

    add function search name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var body = document.body;
 let url = window.location.toString();
 let getName = (url) =>  {
-	let g = url.replace('=');
+	let g = url.split('=');
 	let name = g[1];
 	if (name === undefined) {
 			name = 'saifieva98';
